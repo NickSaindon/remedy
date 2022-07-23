059b8c94b0326d9ca891c0cb7b79e951dbbc61c6
Nick Saindon <ntimeproductions@gmail.com>
2022-07-23 18:26:16 -0400

    Fixed build issues

--- utils/error.js
+++ utils/error.js
@@ -1,13 +1,11 @@
 import db from './db';
 
-const getError = (err) =>
+export const getError = (err) =>
   err.response && err.response.data && err.response.data.message
     ? err.response.data.message
     : err.message;
 
-const onError = async (err, req, res) => {
+export const onError = async (err, req, res) => {
   await db.disconnect();
   res.status(500).send({ message: err.toString() });
 };
-
-export { getError, onError };
