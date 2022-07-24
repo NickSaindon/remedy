e1079cff93153ebd10ba50dbe22a8bfa30246ee2
Nick Saindon <ntimeproductions@gmail.com>
2022-07-23 21:09:38 -0400

    Changed env and db connection

--- utils/db.js
+++ utils/db.js
@@ -2,40 +2,34 @@ import mongoose from 'mongoose';
 
 const connection = {};
 
-const connect = async () => {
-    if (connection.isConnected) {
-        console.log('already connected');
-        return;
+async function connect() {
+  if (connection.isConnected) {
+    console.log('already connected');
+    return;
+  }
+  if (mongoose.connections.length > 0) {
+    connection.isConnected = mongoose.connections[0].readyState;
+    if (connection.isConnected === 1) {
+      console.log('use previous connection');
+      return;
     }
-
-    if (mongoose.connections.length > 0) {
-        connection.isConnected = mongoose.connections[0].readyState;
-        if (connection.isConnected === 1) {
-          console.log('use previous connection');
-          return;
-        }
-        await mongoose.disconnect();
-    }
-
-    const db = mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log('new connection');
-    connection.isConnected = db.connections[0].readyState;
+    await mongoose.disconnect();
+  }
+  const db = await mongoose.connect(process.env.MONGODB_URI);
+  console.log('new connection');
+  connection.isConnected = db.connections[0].readyState;
 }
 
-const disconnect = async () => {
-    if (connection.isConnected) {
-        if (process.env.NODE_ENV == 'production') {
-            await mongoose.disconnect();
-            connection.isConnected = false;
-        } else {
-            console.log('not disconnected');
-        }
+async function disconnect() {
+  if (connection.isConnected) {
+    if (process.env.NODE_ENV === 'production') {
+      await mongoose.disconnect();
+      connection.isConnected = false;
+    } else {
+      console.log('not disconnected');
     }
+  }
 }
-
 function convertDocToObj(doc) {
   doc._id = doc._id.toString();
   doc.createdAt = doc.createdAt.toString();
